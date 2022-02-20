359cc7c3192c954467eb08d8d8663214721de668
Ricardo Sanchez <ristsanchez@gmail.com>
2022-02-19 19:05:56 -0700

    Comment tip

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  //$LATER$ remove top part of app
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
