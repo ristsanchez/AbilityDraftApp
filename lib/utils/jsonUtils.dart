34119765187de6018f9685eca7d5d336ca0e0d49
Ricardo Sanchez <ristsanchez@gmail.com>
2022-02-19 19:06:15 -0700

    Create jsonUtils.dart

---
+++ lib/utils/jsonUtils.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+Future<List<String>> loadHeroNames(BuildContext context) async {
+  String data =
+      await DefaultAssetBundle.of(context).loadString('lib/utils/dota2.json');
+  //starts at the <nameofprojectdirectory> level, in this case "ability_draft"
+  //Then we go to lib/utils and then find the file.
+  Map<String, dynamic> map = json.decode(data);
+  List<String> oops = map.keys.toList();
+  return oops;
+}
