99e0eeb1f3a57577b734a7a6423b2b06275f1c4b
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-02-27 21:39:48 -0700

    Rework

--- lib/utils/jsonUtils.dart
+++ lib/utils/jsonUtils.dart
@@ -11,3 +11,21 @@ Future<List<String>> loadHeroNames(BuildContext context) async {
   List<String> oops = map.keys.toList();
   return oops;
 }
+
+Future<Map<String, dynamic>> loadAllHeroData(BuildContext context) async {
+  String data =
+      await DefaultAssetBundle.of(context).loadString('lib/utils/dota2.json');
+  //starts at the <nameofprojectdirectory> level, in this case "ability_draft"
+  //Then we go to lib/utils and then find the file.
+  Map<String, dynamic> map = json.decode(data);
+  return map;
+}
+
+Future<Map<String, dynamic>> loadEngData(BuildContext context) async {
+  String data = await DefaultAssetBundle.of(context)
+      .loadString('lib/utils/abilities.json');
+  //starts at the <nameofprojectdirectory> level, in this case "ability_draft"
+  //Then we go to lib/utils and then find the file.
+  Map<String, dynamic> map = json.decode(data);
+  return map;
+}
