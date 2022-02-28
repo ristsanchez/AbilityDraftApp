1826884cf5636394f55fd5d33693c6bd83c3ea9b
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-02-27 21:41:24 -0700

    Rework on data reading
    The new methods take data from a list of heroes instead of the hero model entry list.

--- lib/Heroes/HeroesAll.dart
+++ lib/Heroes/HeroesAll.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 
 import 'package:scoped_model/scoped_model.dart';
 
+import '../utils/jsonUtils.dart';
+import 'HeroDialogs/HeroStatsDialog.dart';
 import 'HeroesModel.dart';
 import 'AHero.dart';
 
@@ -19,37 +21,84 @@ class HeroesAll extends StatelessWidget {
         builder: (BuildContext context, Widget child, HeroesModel model) {
       return Scaffold(
         body: Center(
-          child: _buildContents(context, model),
-        ),
+            child: FutureBuilder(
+          future: _allHeroData(context),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            List<Widget> children;
+            if (snapshot.hasData) {
+              return _buildContents(context, snapshot.data);
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                )
+              ];
+            } else {
+              children = const <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Loading...'),
+                )
+              ];
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: children,
+              ),
+            );
+          },
+        )
+
+            // _buildContents(context, model),
+            ),
         floatingActionButton: FloatingActionButton(
             child: const Icon(Icons.next_plan, color: Colors.white),
             onPressed: () async {
               model.setStackIndex(1);
-
-              // Map<String, dynamic> tempPress;
-              // tempPress = await loadAllHeroData(context);
-              // initializeDB(context, tempPress);
             }),
       );
     });
   }
-
-  //$CHECK$Do this when the app is opened
-  initializeDB(BuildContext context, Map<String, dynamic> data) {
-    AHero temp;
-    data.forEach((key, value) async {
-      temp = AHero.fromJson(value, key.toString());
-      await HeroesDBWorker.db.create(temp);
-    });
-    Scaffold.of(context).showSnackBar(const SnackBar(
-      backgroundColor: Colors.green,
-      duration: Duration(seconds: 1),
-      content: Text('Success'),
-    ));
-  }
 }
 
-_buildContents(BuildContext context, HeroesModel model) {
+Future<List<AHero>> _allHeroData(BuildContext context) async {
+  String data = await DefaultAssetBundle.of(context)
+      .loadString('lib/gameData/HeroesCond.json');
+  //starts at the <nameofprojectdirectory> level, in this case "ability_draft"
+  //Then we go to lib/utils and then find the file.
+  Map<String, dynamic> map = json.decode(data);
+  List<AHero> list = [];
+  map.forEach((k, v) => list.add(AHero.fromJson(k, v)));
+  return list;
+}
+
+//$CHECK$Do this when the app is opened
+// _initializeDB(BuildContext context, Map<String, dynamic> data) {
+//   AHero temp;
+//   data.forEach((key, value) async {
+//     temp = AHero.fromJson(value, key.toString());
+//     await HeroesDBWorker.db.create(temp);
+//   });
+//   Scaffold.of(context).showSnackBar(const SnackBar(
+//     backgroundColor: Colors.green,
+//     duration: Duration(seconds: 1),
+//     content: Text('Success'),
+//   ));
+// }
+
+_buildContents(BuildContext context, List<AHero> heroList) {
   return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints viewportConstraints) {
     return SingleChildScrollView(
@@ -61,24 +110,36 @@ _buildContents(BuildContext context, HeroesModel model) {
           child: Column(children: <Widget>[
             Container(
               // A fixed-height child.
-              color: const Color(0xffeeee00), // Yellow
+              color: Colors.black, // Yellow
               height: 120.0,
               alignment: Alignment.center,
 
               //get all from db, count it, display it as string
-              child: CachedNetworkImage(
-                imageUrl:
-                    'http://cdn.dota2.com/apps/dota2/images/heroes/axe_lg.png',
-                placeholder: (context, url) =>
-                    const CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Text(error.toString()),
+              child: GestureDetector(
+                onTap: () async {
+                  Scaffold.of(context).showSnackBar(const SnackBar(
+                    backgroundColor: Colors.blueAccent,
+                    duration: Duration(seconds: 2),
+                    content: Text('Working'),
+                  ));
+                  // Map<String, dynamic> tempPress;
+                  // tempPress = await loadAllHeroData(context);
+                  //_initializeDB(context, tempPress);
+                },
+                child: CachedNetworkImage(
+                  imageUrl:
+                      'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/axe.png',
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => Text(error.toString()),
+                ),
               ),
             ),
             Expanded(
               // A flexible child that will grow to fit the viewport but
               // still be at least as big as necessary to fit its contents.
               child: Container(
-                color: Colors.blueGrey, // Red
+                color: Colors.black, // Red
                 height: 120.0,
                 alignment: Alignment.center,
                 child: GridView.builder(
@@ -86,21 +147,21 @@ _buildContents(BuildContext context, HeroesModel model) {
                     padding: const EdgeInsets.all(5),
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
-                      childAspectRatio: 1.66,
+                      childAspectRatio: 1.78,
                       crossAxisCount: 3,
                     ),
-                    itemCount: model.entryList.length,
+                    itemCount: heroList.length,
                     itemBuilder: (BuildContext context, int index) {
                       return GestureDetector(
                         onTap: () {
-                          _showHeroDialog(context, model.entryList[index]);
+                          showHeroDialog(context, heroList[index]);
                         },
                         child: Stack(children: <Widget>[
                           ClipRRect(
                             borderRadius: BorderRadius.circular(5.0),
                             child: CachedNetworkImage(
                               imageUrl:
-                                  'http://cdn.dota2.com/apps/dota2/images/heroes/${model.entryList[index].base_name}_lg.png',
+                                  'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${heroList[index].base_name}.png',
                               placeholder: (context, url) =>
                                   const CircularProgressIndicator(),
                               //$LATER$ make sure progess indicator appears centered and right size
@@ -115,7 +176,7 @@ _buildContents(BuildContext context, HeroesModel model) {
                               child: RichText(
                                 textAlign: TextAlign.left,
                                 text: TextSpan(
-                                  text: model.entryList[index].name ?? "empty",
+                                  text: heroList[index].name ?? "empty",
                                   style: const TextStyle(
                                       color: Colors.white,
                                       fontWeight: FontWeight.bold),
@@ -134,242 +195,6 @@ _buildContents(BuildContext context, HeroesModel model) {
       ),
     );
   });
-}
-
-_tempCreateStats(HeroesModel model, BuildContext context) {
-  // List<AHero> temst = [];
-  // temst = await HeroesDBWorker.db.getAll();
-
-  Scaffold.of(context).showSnackBar(SnackBar(
-    backgroundColor: Colors.green,
-    duration: const Duration(seconds: 1),
-    content: Text(model.entryList.length.toString()),
-  ));
-}
-
-Color? dom = Colors.grey[800];
-Color? lig = Colors.grey[700];
-
-_showHeroDialog(BuildContext context, AHero hero) {
-  return showDialog(
-    context: context,
-    builder: (context) => SimpleDialog(
-      backgroundColor: lig,
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(12.0))),
-      contentPadding: const EdgeInsets.fromLTRB(0, 12, 0, 10),
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.fromLTRB(12, 0, 0, 12),
-              color: lig,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  SizedBox(
-                    height: 60,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: CachedNetworkImage(
-                        imageUrl:
-                            'http://cdn.dota2.com/apps/dota2/images/heroes/${hero.base_name}_lg.png',
-                        placeholder: (context, url) =>
-                            const CircularProgressIndicator(),
-                        //$LATER$ make sure progess indicator appears centered and right size
-                        errorWidget: (context, url, error) =>
-                            Text(error.toString()),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        RichText(
-                          textAlign: TextAlign.left,
-                          text: TextSpan(
-                            text: hero.name,
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18),
-                          ),
-                        ),
-                        RichText(
-                          textAlign: TextAlign.left,
-                          text: TextSpan(
-                            text: primaryAtt(hero.primary_attr ?? ""),
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        RichText(
-                          textAlign: TextAlign.left,
-                          text: TextSpan(
-                            text: heroType(hero.type),
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              color: dom,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: <Widget>[
-                        const Icon(
-                          Icons.sensor_window_rounded,
-                          color: Colors.red,
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text:
-                                '${hero.base_str} + ${hero.str_per_level!.substring(0, 3)}',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: (hero.primary_attr == 'str')
-                                    ? FontWeight.bold
-                                    : FontWeight.normal),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: <Widget>[
-                        const Icon(
-                          Icons.upcoming_sharp,
-                          color: Colors.green,
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text:
-                                '${hero.base_agi} + ${hero.agi_per_level!.substring(0, 3)}',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: (hero.primary_attr == 'agi')
-                                    ? FontWeight.bold
-                                    : FontWeight.normal),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: <Widget>[
-                        const Icon(
-                          Icons.grain_sharp,
-                          color: Colors.blue,
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text:
-                                '${hero.base_int} + ${hero.int_per_level!.substring(0, 3)}',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: (hero.primary_attr == 'int')
-                                    ? FontWeight.bold
-                                    : FontWeight.normal),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(12, 10, 0, 0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      RichText(
-                        textAlign: TextAlign.left,
-                        text: TextSpan(
-                          text: 'Move-speed: ${hero.base_movement_speed}',
-                          style: const TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      const VerticalDivider(
-                        width: 30,
-                      ),
-                      RichText(
-                        textAlign: TextAlign.left,
-                        text: TextSpan(
-                          text:
-                              'Armor: ${heroArmor(hero.base_armor, hero.base_agi).toStringAsFixed(1)}',
-                          style: const TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  RichText(
-                    textAlign: TextAlign.left,
-                    text: TextSpan(
-                      text:
-                          'Damage: ${hero.base_damage_min}-${hero.base_damage_max}',
-                      style: const TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const Divider(
-                    height: 4,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ],
-    ),
-  );
-}
-/*
-$LATER$ make 3 rows in the last column 2 rows with a double entry
-
--
--
--
- 
-- -
-- 
-
-*/
-
-double heroArmor(String? base, int? agi) {
-  return double.parse(base!) + (agi! / 6);
-}
-
-heroType(String? t) {
-  return '${t!.substring(0, 1).toUpperCase()}${t.substring(1, t.length)}';
 }
 
 primaryAtt(String att) {
