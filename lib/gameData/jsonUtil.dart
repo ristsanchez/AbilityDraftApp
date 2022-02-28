bb8f441b5ecae746c0b378a20c0502b95108df2c
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-02-27 21:38:22 -0700

    Create json util methods

---
+++ lib/gameData/jsonUtil.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+Future<List<String>> loadHeroNames(BuildContext context) async {
+  String data =
+      await DefaultAssetBundle.of(context).loadString('lib/utils/dota2.json');
+  //starts at the <nameofprojectdirectory> level, in this case "ability_draft"
+  //Then we go to lib/utils and then find the file.
+  Map<String, dynamic> map = json.decode(data);
+  List<String> oops = map.keys.toList();
+  return oops;
+}
+
+Future<Map<String, dynamic>> heroData(BuildContext context) async {
+  String data =
+      await DefaultAssetBundle.of(context).loadString('lib/utils/dota2.json');
+  //starts at the <nameofprojectdirectory> level, in this case "ability_draft"
+  //Then we go to lib/utils and then find the file.
+  Map<String, dynamic> map = json.decode(data);
+  return map;
+}
+
+Future<Map<String, dynamic>> loadEngData(BuildContext context) async {
+  String data = await DefaultAssetBundle.of(context)
+      .loadString('lib/utils/abilities.json');
+  //starts at the <nameofprojectdirectory> level, in this case "ability_draft"
+  //Then we go to lib/utils and then find the file.
+  Map<String, dynamic> map = json.decode(data);
+  return map;
+}
