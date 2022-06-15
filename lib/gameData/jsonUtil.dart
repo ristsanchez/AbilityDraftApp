7bff182b586e05acc6b51bc436d744df414aa9c5
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-15 00:15:16 -0600

    Add NameById
    Created a new future method that retrieves just the name of an ability based on an Id.

--- lib/gameData/jsonUtil.dart
+++ lib/gameData/jsonUtil.dart
@@ -29,3 +29,12 @@ Future<Map<String, dynamic>> loadEngData(BuildContext context) async {
   Map<String, dynamic> map = json.decode(data);
   return map;
 }
+
+Future<String> getAbilityNameById(BuildContext context, int id) async {
+  String data = await DefaultAssetBundle.of(context)
+      .loadString('lib/utils/abiIdTable.json');
+  //starts at the <nameofprojectdirectory> level, in this case "ability_draft"
+  //Then we go to lib/utils and then find the file.
+  Map<String, dynamic> map = json.decode(data);
+  return map[id.toString()] ?? 'Error';
+}
