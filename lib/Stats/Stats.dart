3ab61851a142428fc2632722ecccf0a609691dda
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-17 00:08:25 -0600

    Create test tab

---
+++ lib/Stats/Stats.dart
@@ -0,0 +1,27 @@
+import 'package:ability_draft/Matches/ScoreboardScreen/ScoreboardBig.dart';
+import 'package:flutter/material.dart';
+
+class Stats extends StatelessWidget {
+  const Stats({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 230, 203, 122),
+      body: Center(
+        child: Container(
+          color: Colors.deepPurple,
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ScoreboardBig()));
+            },
+            child: Text('gola'),
+          ),
+        ),
+      ),
+    );
+  }
+}
