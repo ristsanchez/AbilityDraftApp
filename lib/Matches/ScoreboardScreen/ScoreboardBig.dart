82aa5a5a9d27a95f0a8da8f418e6420fa99b2325
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-17 23:53:20 -0600

    Create Scoreboardbig
    Created new page in new route to display whole scoreboard from detailed match data

--- lib/Matches/ScoreboardScreen/ScoreboardBig.dart
+++ lib/Matches/ScoreboardScreen/ScoreboardBig.dart
@@ -1,27 +1,177 @@
+import 'package:ability_draft/Matches/MatchData/match_entry.dart';
+import 'package:ability_draft/Matches/MatchData/player_entry.dart';
+import 'package:ability_draft/utils/idTable.dart';
 import 'package:flutter/material.dart';
 
 class ScoreboardBig extends StatelessWidget {
-  const ScoreboardBig({Key? key}) : super(key: key);
+  final MatchEntry matchData;
+  const ScoreboardBig({
+    Key? key,
+    required this.matchData,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Scoreboard'),
+        toolbarHeight: 40,
+        title: const Text('Scoreboard'),
         centerTitle: true,
       ),
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.black87,
       body: Center(
-        child: Container(
-          color: Colors.green,
-          child: TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Scoreboard'),
-          ),
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: getScoreboard(matchData.playerList)),
+          ],
         ),
       ),
     );
   }
+}
+
+getScoreboard(List<Player> playerList) {
+  List<Widget> sbWidgetList = [];
+
+  for (Player each in playerList) {
+    if (each.isRadiant) {
+      sbWidgetList.insert(0, rowMaker(each));
+    } else {
+      sbWidgetList.insert(sbWidgetList.length - 1, rowMaker(each));
+    }
+  }
+
+  sbWidgetList.insert(0, getHeroMatchDataHeader(true));
+  sbWidgetList.insert(6, getHeroMatchDataHeader(false));
+  sbWidgetList.insert(0, SizedBox(height: 5));
+  return sbWidgetList;
+}
+
+getHeroMatchDataHeader(bool isRadiant) {
+  return Container(
+    height: 40,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: 100,
+          child: Text(
+            isRadiant ? 'Radiant' : 'Dire',
+            style: TextStyle(
+              color: isRadiant ? Colors.green : Colors.red,
+              fontSize: 16,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        colChar('K'),
+        colChar('D'),
+        colChar('A'),
+        colChar('NET',
+            special: true, width: 50), //after this: itemsbackpackbuffs
+        colChar('LH'),
+        colChar('/', width: 10),
+        colChar('DN'),
+        colChar('GPM', special: true, width: 50),
+        colChar('XPM', width: 40),
+        colChar('DMG', width: 50),
+        colChar('HEAL', width: 50),
+        colChar('BLD', width: 50),
+        const SizedBox(
+          width: 32,
+        )
+      ],
+    ),
+  );
+}
+
+//Makes a container with the according column name, size and color
+colChar(String name, {bool special = false, int width = 30}) {
+  return Container(
+    alignment: Alignment.center,
+    width: width.toDouble(),
+    child: Text(
+      name,
+      style: TextStyle(
+        color: !special
+            ? const Color.fromARGB(200, 255, 255, 255)
+            : const Color.fromARGB(200, 255, 223, 0),
+        fontSize: 16,
+      ),
+    ),
+  );
+}
+
+rowMaker(Player player) {
+  return Container(
+    margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+    color: const Color.fromARGB(45, 255, 255, 255),
+    child: Row(
+      // direction: Axis.horizontal,
+      mainAxisAlignment: MainAxisAlignment.start,
+      // alignment: WrapAlignment.spaceEvenly,
+      children: [
+        Image.asset(
+          'assets/hero_icons_small/${getNameById(player.heroId.toString())}.jpg',
+          fit: BoxFit.fill,
+          height: 40,
+          width: 40,
+        ),
+        Container(
+          width: 50,
+          child: Container(
+            width: 30,
+            height: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                  width: 2, color: const Color.fromARGB(255, 212, 184, 55)),
+            ),
+            child: Center(
+                child: Text(
+              '27',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 212, 184, 55), fontSize: 12),
+            )),
+          ),
+        ),
+        colEntry(player.kills),
+        colEntry(player.deaths),
+        colEntry(player.assists),
+        colEntry(player.netWorth, special: true, width: 50),
+        colEntry(player.lastHits),
+        colChar('/', width: 10),
+        colEntry(player.denies),
+        colEntry(player.goldPerMin, special: true, width: 50),
+        colEntry(player.xpPerMin, width: 40),
+        colEntry(player.heroDamage, width: 50),
+        colEntry(player.heroHealing, width: 50),
+        colEntry(player.towerDamage, width: 50),
+        const SizedBox(
+          width: 32,
+        )
+      ],
+    ),
+  );
+}
+
+colEntry(int val, {bool special = false, int width = 30}) {
+  return Container(
+    alignment: Alignment.center,
+    width: width.toDouble(),
+    height: 40,
+    child: Text(
+      val.toString(),
+      style: TextStyle(
+        color: !special
+            ? const Color.fromARGB(200, 255, 255, 255)
+            : const Color.fromARGB(200, 255, 223, 0),
+        fontSize: 16,
+      ),
+    ),
+  );
 }
