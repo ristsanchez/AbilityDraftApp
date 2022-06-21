7cbe49fe5d31f48f7405785622a96ebc6ee57801
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-20 22:49:32 -0600

    Add multi provider
    Created providers to be changed into a different part. Putting them at the root of the app is a bad practice.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'package:ability_draft/Abilities/Ability.dart';
+import 'package:ability_draft/Abilities/ability_list_provider.dart';
+import 'package:ability_draft/Abilities/textFilterPr.dart';
 import 'package:ability_draft/Stats/Stats.dart';
 import 'package:flutter/material.dart';
 import 'Abilities/index.dart';
 import 'Heroes/index.dart';
 import 'Matches/index.dart';
 
+import 'package:provider/provider.dart';
+import 'gameData/jsonUtil.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AbilityListProvider()),
+        // ChangeNotifierProvider.value(value: AbilityListProvider()),
+        ChangeNotifierProvider(create: (_) => AbiFilterProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,15 +32,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.grey,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -36,15 +42,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -64,12 +61,10 @@ class _MyHomePageState extends State<MyHomePage>
   ];
   late TabController _tabController;
 
-  // int _counter = 0;
-  // String sample = "";
-
   @override
   void initState() {
     super.initState();
+
     _tabController = TabController(vsync: this, length: _tabList.length);
   }
 
@@ -81,12 +76,6 @@ class _MyHomePageState extends State<MyHomePage>
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: null,
       body: TabBarView(
@@ -113,7 +102,6 @@ class _MyHomePageState extends State<MyHomePage>
           BottomNavigationBarItem(label: ("Stats"), icon: Icon(Icons.dangerous))
         ],
       ),
-// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
