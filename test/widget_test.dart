b5ce8c1f50be6743ccab553e32ea092571b691eb
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-22 00:13:13 -0600

    m

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ability_draft/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(theme: ThemeData.dark()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
