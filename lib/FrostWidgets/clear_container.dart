d226bc922497d138560277f3556c46c0067e2bec
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-23 16:10:49 -0600

    m

--- lib/FrostWidgets/clear_container.dart
+++ lib/FrostWidgets/clear_container.dart
@@ -1,7 +1,28 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-clearContainer(Widget child0) {
+clearContainer(Widget child) {
+  return ClipRRect(
+    borderRadius: BorderRadius.circular(15),
+    child: BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(0, 2, 0, 0),
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.12),
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+          border: Border.all(
+            width: 2,
+            color: Colors.white.withOpacity(0.05),
+          ),
+        ),
+        child: child,
+      ),
+    ),
+  );
+}
+
+clearContainerRect(Widget child0) {
   return ClipRRect(
     borderRadius: BorderRadius.circular(15),
     child: BackdropFilter(
