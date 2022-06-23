f63c31ce28db8f2a14ed7569072e188be3b439ee
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-23 16:13:45 -0600

    Remove redundant 'Expanded'
    Removed expanded widgets and cleaned up some other redundant containers to simplify widget tree.

--- lib/abilities/abilities_home_screen.dart
+++ lib/abilities/abilities_home_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 
+import 'package:ability_draft/FrostWidgets/clear_container.dart';
 import 'package:ability_draft/abilities/ability_objects/ability.dart';
 import 'package:ability_draft/abilities/ability_providers/ability_list_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+const double indentation = 100;
 
 class AbilitiesHome extends StatelessWidget {
   const AbilitiesHome({Key? key}) : super(key: key);
@@ -15,16 +18,11 @@ class AbilitiesHome extends StatelessWidget {
       appBar: AppBar(
         toolbarHeight: 0,
       ),
-      body: Container(
-        color: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            getSearchPartRENAME(context),
-            customGrid(context),
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          customGrid(context),
+          getSearchPartRENAME(context),
+        ],
       ),
     );
   }
@@ -33,34 +31,50 @@ class AbilitiesHome extends StatelessWidget {
 getSearchPartRENAME(BuildContext context) {
   Provider.of<AbilityListProvider>(context, listen: false).list;
   return SizedBox(
-    height: 230,
-    child: Column(
-      children: [
-        SizedBox(
-          height: 59,
-        ),
-        TextField(
-          enableSuggestions: true,
-          // autofillHints:
-          //     Provider.of<AbilityListProvider>(context, listen: false)
-          //         .abilityHintList,
-          onChanged: (value) {
-            Provider.of<AbilityListProvider>(context, listen: false)
-                .setText(value);
-          },
-          cursorColor: Colors.red,
-          obscureText: false,
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Search',
+    height: indentation,
+    // width: MediaQuery.of(context).size.width,
+    child: clearContainer(
+      Column(
+        children: [
+          SizedBox(
+            height: 10,
           ),
-        ),
-        Container(
-          child: Text(
-            Provider.of<AbilityListProvider>(context).list.length.toString(),
+          SizedBox(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .75,
+                  child: TextField(
+                    enableSuggestions: true,
+                    // autofillHints:
+                    //     Provider.of<AbilityListProvider>(context, listen: false)
+                    //         .abilityHintList,
+                    onChanged: (value) {
+                      Provider.of<AbilityListProvider>(context, listen: false)
+                          .setText(value);
+                    },
+                    cursorColor: Colors.red,
+                    obscureText: false,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Search',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 1,
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+          Container(
+            child: Text(
+              Provider.of<AbilityListProvider>(context).list.length.toString(),
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
@@ -72,62 +86,58 @@ customGrid(BuildContext context) {
 
   return abilityList.isEmpty
       ? smallProg()
-      : Expanded(
-          child: GridView.builder(
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(5),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              mainAxisSpacing: 5,
-              crossAxisSpacing: 5,
-              childAspectRatio: 1,
-              crossAxisCount: 4,
-            ),
-            itemCount: Provider.of<AbilityListProvider>(context).list.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                  // showAbilityDialog(context, abilityList[index]);
-                },
-                child: Stack(
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(15.0),
-                      child: Image.asset(
-                        'assets/ability_images/${Provider.of<AbilityListProvider>(context).list[index].name}.png',
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(5),
-                      child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          Provider.of<AbilityListProvider>(context)
-                              .list[index]
-                              .id,
-                          style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
+      : GridView.builder(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(5, indentation, 5, 5),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+            childAspectRatio: 1,
+            crossAxisCount: 4,
           ),
+          itemCount: Provider.of<AbilityListProvider>(context).list.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                // showAbilityDialog(context, abilityList[index]);
+              },
+              child: Stack(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image.asset(
+                      'assets/ability_images/${Provider.of<AbilityListProvider>(context).list[index].name}.png',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Text(
+                        Provider.of<AbilityListProvider>(context)
+                            .list[index]
+                            .id,
+                        style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         );
 }
 
 smallProg() {
-  return Expanded(
-    child: Container(
-      alignment: Alignment.center,
-      child: const CircularProgressIndicator(color: Colors.red),
-      height: 50,
-      width: 50,
-    ),
+  return Container(
+    alignment: Alignment.center,
+    child: const CircularProgressIndicator(color: Colors.red),
+    height: 50,
+    width: 50,
   );
 }
 
