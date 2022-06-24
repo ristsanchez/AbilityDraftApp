d6b4181a211a5063995ca74d40a9ef671018a6ce
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-23 23:17:19 -0600

    Rework main widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'dart:convert';
 
+import 'package:ability_draft/firebase_options.dart';
+import 'package:ability_draft/google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:json_theme/json_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:ability_draft/Stats/Stats.dart';
 import 'package:ability_draft/abilities/ability_providers/ability_list_provider.dart';
 
-import 'abilities/index.dart';
-import 'heroes/index.dart';
 import 'matches/ability_path_provider.dart';
-import 'matches/index.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final themeStr =
       await rootBundle.loadString('assets/themes/ad_dark_theme.json');
   final themeJson = jsonDecode(themeStr);
@@ -27,87 +29,7 @@ void main() async {
         ChangeNotifierProvider(create: (_) => AbilityListProvider()),
         ChangeNotifierProvider(create: (_) => AbilityPathProvider()),
       ],
-      child: MyApp(theme: theme),
+      child: GoogleSignInRename(theme: theme),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  final ThemeData theme;
-  const MyApp({Key? key, required this.theme}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: theme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  int _currentIndex = 0;
-
-  final List<Widget> _tabList = [
-    const MatchesHome(),
-    const Stats(),
-    const AbilitiesHome(),
-    const HeroesHome(),
-  ];
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _tabController = TabController(vsync: this, length: _tabList.length);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      body: TabBarView(
-        physics: const NeverScrollableScrollPhysics(),
-        controller: _tabController,
-        children: _tabList,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (currentIndex) {
-          setState(() {
-            _currentIndex = currentIndex;
-          });
-          _tabController.animateTo(_currentIndex);
-        },
-        items: const [
-          BottomNavigationBarItem(label: ("Matches"), icon: Icon(Icons.list)),
-          BottomNavigationBarItem(
-              label: ("Stats"), icon: Icon(Icons.query_stats)),
-          BottomNavigationBarItem(label: ("Abilities"), icon: Icon(Icons.apps)),
-          BottomNavigationBarItem(
-              label: ("Heroes"), icon: FaIcon(FontAwesomeIcons.userAstronaut)),
-        ],
-      ),
-    );
-  }
 }
