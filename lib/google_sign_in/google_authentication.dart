dba34c97617fc9b83afcc3681590abd1f33dd9e7
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-24 23:58:06 -0600

    Create auth dart file

---
+++ lib/google_sign_in/google_authentication.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../home_screen.dart';
+
+Future<void> signup(BuildContext context) async {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+
+  if (googleSignInAccount != null) {
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    final AuthCredential authCredential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    // Getting users credential
+    UserCredential result = await auth.signInWithCredential(authCredential);
+    User? user = result.user;
+
+    if (result != null) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const MyHomePage()));
+    } // if result not null we simply call the MaterialpageRoute,
+    // for go to the HomePage screen
+  }
+}
