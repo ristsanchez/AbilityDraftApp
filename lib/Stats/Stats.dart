47993e7ab2eda57dbbff9a5788f2f7720fdf75de
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-26 22:18:25 -0600

    Update imports

--- lib/Stats/Stats.dart
+++ lib/Stats/Stats.dart
@@ -1,7 +1,4 @@
-import 'package:ability_draft/Stats/provider_delete.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Stats extends StatelessWidget {
   const Stats({Key? key}) : super(key: key);
