cb08e4d04c585bf56dc405e8abf0b4f2b7399c39
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-26 22:11:11 -0600

    Refactor const

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,15 +2,15 @@ import 'dart:ui';
 
 //  10 bright and distinct colors,
 //  each to be used for a hero line in a single graph
-final List<Color> heroGraphColors = [
-  const Color(0xFF9E2EE9),
-  const Color(0xFFe6194B),
-  const Color(0xFFf58231),
-  const Color(0xFFffe119),
-  const Color(0xFFaaffc3),
-  const Color(0xFFfabed4),
-  const Color(0xFF3cb44b),
-  const Color(0xFF42d4f4),
-  const Color(0xFF4C71F5),
-  const Color(0xFFf032e6),
+const List<Color> heroGraphColors = [
+  Color(0xFF9E2EE9),
+  Color(0xFFe6194B),
+  Color(0xFFf58231),
+  Color(0xFFffe119),
+  Color(0xFFaaffc3),
+  Color(0xFFfabed4),
+  Color(0xFF3cb44b),
+  Color(0xFF42d4f4),
+  Color(0xFF4C71F5),
+  Color(0xFFf032e6),
 ];
