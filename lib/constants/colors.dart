09e08fe7d1afba15130c8d96241e38c797fa1d69
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-26 21:00:06 -0600

    Create colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+//  10 bright and distinct colors,
+//  each to be used for a hero line in a single graph
+final List<Color> heroGraphColors = [
+  const Color(0xFF9E2EE9),
+  const Color(0xFFe6194B),
+  const Color(0xFFf58231),
+  const Color(0xFFffe119),
+  const Color(0xFFaaffc3),
+  const Color(0xFFfabed4),
+  const Color(0xFF3cb44b),
+  const Color(0xFF42d4f4),
+  const Color(0xFF4C71F5),
+  const Color(0xFFf032e6),
+];
