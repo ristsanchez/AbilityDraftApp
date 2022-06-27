9057c8dbd5a67e02b2d3a15ce15214c65982555b
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-26 22:10:59 -0600

    Refactor and add method
    Renamed file from jsonUtils.dart to json_utils.dart.
    Moved file to new directory.
    Add getAllHeroInfoList, returns a future after reading the hero json file.

--- lib/utility_futures/json_utils.dart
+++ lib/utility_futures/json_utils.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:ability_draft/heroes/heroes_objects/hero.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<List<String>> loadHeroNames(BuildContext context) async {
   String data =
@@ -28,4 +30,14 @@ Future<Map<String, dynamic>> loadEngData(BuildContext context) async {
   //Then we go to lib/utils and then find the file.
   Map<String, dynamic> map = json.decode(data);
   return map;
+}
+
+//---recent---
+//HERO HOME SCREEN UTIL
+Future<List<AHero>> getAllHeroInfoList() async {
+  String data = await rootBundle.loadString('lib/gameData/HeroesCond.json');
+  Map<String, dynamic> map = json.decode(data);
+  List<AHero> list = [];
+  map.forEach((k, v) => list.add(AHero.fromJson(k, v, [])));
+  return list;
 }
