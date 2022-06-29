42a4970d517d335caf3a24f49b84b0c9fad8db99
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-28 23:39:56 -0600

    Add list tile support
    Created the methods to build a list tile containing the relevant match data.
    The fields include, match id, region, time, result, duration, etc.
    
    Started the ground work to add hero images and other important  data when a user taps on an entry.
    
    This tile will expand to show the images.
    
    IT will also have a slidable property, sort of like how gmail has archive.

--- lib/Stats/Stats.dart
+++ lib/Stats/Stats.dart
@@ -1,5 +1,9 @@
 import 'package:ability_draft/Stats/steam_login_webview.dart';
+import 'package:ability_draft/matches/match_objects/index.dart';
+import 'package:ability_draft/utility_futures/json_utils.dart';
+import 'package:ability_draft/utils/idTable.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Stats extends StatelessWidget {
   const Stats({Key? key}) : super(key: key);
@@ -11,14 +15,112 @@ class Stats extends StatelessWidget {
         toolbarHeight: 0,
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => const WebViewApp()));
+        child: FutureBuilder(
+          future: getTestMatchInfo(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    getMatchEntriesHeaders(context),
+                    getMatchEntry(context, snapshot.data),
+                  ],
+                ),
+              );
+            }
+            return CircularProgressIndicator();
           },
-          child: const Text('Sign in with steam'),
         ),
       ),
     );
   }
+}
+
+getMatchEntriesHeaders(BuildContext context) {
+  return SizedBox(
+    width: MediaQuery.of(context).size.width,
+    child: ListTile(
+      trailing: Icon(
+        Icons.abc,
+        color: Color.fromARGB(0, 0, 0, 0),
+      ),
+      title: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Match Id'),
+          Text('Game Mode'),
+          Text('Result'),
+        ],
+      ),
+    ),
+  );
+}
+
+//scrollable row/listview
+
+getMatchEntry(BuildContext context, MatchEntry match) {
+  return ExpansionTile(
+    textColor: Colors.white,
+    title: getLabels(context, match),
+    children: [
+      getTeamImageRow(context, match.getRadiantPlayers()),
+      getTeamImageRow(context, match.getDirePlayers()),
+    ],
+  );
+}
+
+getLabels(BuildContext context, MatchEntry match) {
+  return SizedBox(
+    width: MediaQuery.of(context).size.width,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              match.matchId.toString(),
+              style: TextStyle(color: Colors.blue),
+            ),
+            Text('3 minutes ago')
+          ],
+        ), //MatchID
+
+        Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('Single Draft'), Text('Ranked')]), //GAMEMODE
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Radiant vict',
+            style: TextStyle(color: Colors.red),
+          ),
+          Text('37:52')
+        ]), //REsult/duration
+      ],
+    ),
+  );
+}
+
+getTeamImageRow(BuildContext context, List<Player> playerList) {
+  List<Widget> list = [];
+  playerList.forEach((player) {
+    list.add(
+      Image.asset(
+        'assets/hero_portraits_big/${getNameById(player.heroId)}.png',
+        fit: BoxFit.fill,
+        width: MediaQuery.of(context).size.width / 5,
+      ),
+    );
+  });
+
+  return SizedBox(
+    child: Row(
+      children: list,
+      mainAxisAlignment: MainAxisAlignment.center,
+    ),
+  );
 }
