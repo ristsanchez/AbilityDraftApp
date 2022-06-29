f4f1b8c523fb2436762ce9a829aa695191868206
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-06-28 23:36:33 -0600

    Update id parameter
    Replaced case statements to accept int instead of string.
    
    This is because it makes more sense to pass it directly instead of making redundant conversions.

--- lib/utils/idTable.dart
+++ lib/utils/idTable.dart
@@ -1,250 +1,250 @@
-getNameById(String id) {
+getNameById(int id) {
   switch (id) {
-    case "1":
+    case 1:
       return "antimage";
-    case "2":
+    case 2:
       return "axe";
-    case "3":
+    case 3:
       return "bane";
-    case "4":
+    case 4:
       return "bloodseeker";
-    case "5":
+    case 5:
       return "crystal_maiden";
-    case "6":
+    case 6:
       return "drow_ranger";
-    case "7":
+    case 7:
       return "earthshaker";
-    case "8":
+    case 8:
       return "juggernaut";
-    case "9":
+    case 9:
       return "mirana";
-    case "11":
+    case 11:
       return "nevermore";
-    case "10":
+    case 10:
       return "morphling";
-    case "12":
+    case 12:
       return "phantom_lancer";
-    case "13":
+    case 13:
       return "puck";
-    case "14":
+    case 14:
       return "pudge";
-    case "15":
+    case 15:
       return "razor";
-    case "16":
+    case 16:
       return "sand_king";
-    case "17":
+    case 17:
       return "storm_spirit";
-    case "18":
+    case 18:
       return "sven";
-    case "19":
+    case 19:
       return "tiny";
-    case "20":
+    case 20:
       return "vengefulspirit";
-    case "21":
+    case 21:
       return "windrunner";
-    case "22":
+    case 22:
       return "zuus";
-    case "23":
+    case 23:
       return "kunkka";
-    case "25":
+    case 25:
       return "lina";
-    case "31":
+    case 31:
       return "lich";
-    case "26":
+    case 26:
       return "lion";
-    case "27":
+    case 27:
       return "shadow_shaman";
-    case "28":
+    case 28:
       return "slardar";
-    case "29":
+    case 29:
       return "tidehunter";
-    case "30":
+    case 30:
       return "witch_doctor";
-    case "32":
+    case 32:
       return "riki";
-    case "33":
+    case 33:
       return "enigma";
-    case "34":
+    case 34:
       return "tinker";
-    case "35":
+    case 35:
       return "sniper";
-    case "36":
+    case 36:
       return "necrolyte";
-    case "37":
+    case 37:
       return "warlock";
-    case "38":
+    case 38:
       return "beastmaster";
-    case "39":
+    case 39:
       return "queenofpain";
-    case "40":
+    case 40:
       return "venomancer";
-    case "41":
+    case 41:
       return "faceless_void";
-    case "42":
+    case 42:
       return "skeleton_king";
-    case "43":
+    case 43:
       return "death_prophet";
-    case "44":
+    case 44:
       return "phantom_assassin";
-    case "45":
+    case 45:
       return "pugna";
-    case "46":
+    case 46:
       return "templar_assassin";
-    case "47":
+    case 47:
       return "viper";
-    case "48":
+    case 48:
       return "luna";
-    case "49":
+    case 49:
       return "dragon_knight";
-    case "50":
+    case 50:
       return "dazzle";
-    case "51":
+    case 51:
       return "rattletrap";
-    case "52":
+    case 52:
       return "leshrac";
-    case "53":
+    case 53:
       return "furion";
-    case "54":
+    case 54:
       return "life_stealer";
-    case "55":
+    case 55:
       return "dark_seer";
-    case "56":
+    case 56:
       return "clinkz";
-    case "57":
+    case 57:
       return "omniknight";
-    case "58":
+    case 58:
       return "enchantress";
-    case "59":
+    case 59:
       return "huskar";
-    case "60":
+    case 60:
       return "night_stalker";
-    case "61":
+    case 61:
       return "broodmother";
-    case "62":
+    case 62:
       return "bounty_hunter";
-    case "63":
+    case 63:
       return "weaver";
-    case "64":
+    case 64:
       return "jakiro";
-    case "65":
+    case 65:
       return "batrider";
-    case "66":
+    case 66:
       return "chen";
-    case "67":
+    case 67:
       return "spectre";
-    case "69":
+    case 69:
       return "doom_bringer";
-    case "68":
+    case 68:
       return "ancient_apparition";
-    case "70":
+    case 70:
       return "ursa";
-    case "71":
+    case 71:
       return "spirit_breaker";
-    case "72":
+    case 72:
       return "gyrocopter";
-    case "73":
+    case 73:
       return "alchemist";
-    case "74":
+    case 74:
       return "invoker";
-    case "75":
+    case 75:
       return "silencer";
-    case "76":
+    case 76:
       return "obsidian_destroyer";
-    case "77":
+    case 77:
       return "lycan";
-    case "78":
+    case 78:
       return "brewmaster";
-    case "79":
+    case 79:
       return "shadow_demon";
-    case "80":
+    case 80:
       return "lone_druid";
-    case "81":
+    case 81:
       return "chaos_knight";
-    case "82":
+    case 82:
       return "meepo";
-    case "83":
+    case 83:
       return "treant";
-    case "84":
+    case 84:
       return "ogre_magi";
-    case "85":
+    case 85:
       return "undying";
-    case "86":
+    case 86:
       return "rubick";
-    case "87":
+    case 87:
       return "disruptor";
-    case "88":
+    case 88:
       return "nyx_assassin";
-    case "89":
+    case 89:
       return "naga_siren";
-    case "90":
+    case 90:
       return "keeper_of_the_light";
-    case "91":
+    case 91:
       return "wisp";
-    case "92":
+    case 92:
       return "visage";
-    case "93":
+    case 93:
       return "slark";
-    case "94":
+    case 94:
       return "medusa";
-    case "95":
+    case 95:
       return "troll_warlord";
-    case "96":
+    case 96:
       return "centaur";
-    case "97":
+    case 97:
       return "magnataur";
-    case "98":
+    case 98:
       return "shredder";
-    case "99":
+    case 99:
       return "bristleback";
-    case "100":
+    case 100:
       return "tusk";
-    case "101":
+    case 101:
       return "skywrath_mage";
-    case "102":
+    case 102:
       return "abaddon";
-    case "103":
+    case 103:
       return "elder_titan";
-    case "104":
+    case 104:
       return "legion_commander";
-    case "106":
+    case 106:
       return "ember_spirit";
-    case "107":
+    case 107:
       return "earth_spirit";
-    case "109":
+    case 109:
       return "terrorblade";
-    case "110":
+    case 110:
       return "phoenix";
-    case "111":
+    case 111:
       return "oracle";
-    case "105":
+    case 105:
       return "techies";
-    case "112":
+    case 112:
       return "winter_wyvern";
-    case "113":
+    case 113:
       return "arc_warden";
-    case "108":
+    case 108:
       return "abyssal_underlord";
-    case "114":
+    case 114:
       return "monkey_king";
-    case "120":
+    case 120:
       return "pangolier";
-    case "119":
+    case 119:
       return "dark_willow";
-    case "121":
+    case 121:
       return "grimstroke";
-    case "129":
+    case 129:
       return "mars";
-    case "126":
+    case 126:
       return "void_spirit";
-    case "128":
+    case 128:
       return "snapfire";
-    case "123":
+    case 123:
       return "hoodwink";
-    case "135":
+    case 135:
       return "dawnbreaker";
-    case "136":
+    case 136:
       return "marci";
-    case "137":
+    case 137:
       return "primal_beast";
   }
   return '';
