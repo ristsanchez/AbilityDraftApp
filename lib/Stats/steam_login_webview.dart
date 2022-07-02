f80ba2c1968b409a25e70103af21317b15575518
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-01 22:58:50 -0600

    Remove legacy webview
    Removed legacy webview to be replaced with the newer webview which is currently maintained.

--- lib/Stats/steam_login_webview.dart
+++ lib/Stats/steam_login_webview.dart
@@ -40,24 +40,11 @@ class _WebViewAppState extends State<WebViewApp> {
 }
 
 class SteamLogin extends StatelessWidget {
-  final _webView = FlutterWebviewPlugin();
 
   @override
   Widget build(BuildContext context) {
     // Listen to the onUrlChanged events, and when we are ready to validate do so.
-    _webView.onUrlChanged.listen((String url) async {
-      var openId = OpenId.fromUri(Uri.parse(url));
-      if (openId.mode == 'id_res') {
-        await _webView.close();
-        Navigator.of(context).pop(openId.validate());
-      }
-    });
 
     var openId = OpenId.raw('https://myapp', 'https://myapp', {});
-    return WebviewScaffold(
-        url: openId.authUrl().toString(),
-        appBar: AppBar(
-          title: Text('Steam Login'),
-        ));
   }
 }
