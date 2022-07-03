fab5c5af6aa289421f4c1cd4fb31e6f67f811a78
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-02 23:18:13 -0600

    Add epoch method
    The method getTimeAgoEpoch receives a unix time and returns the corresponding time since the event.
    
    For example, if an event happened x seconds ago, it returns 2 hours ago.

--- lib/utility_time/time_utils.dart
+++ lib/utility_time/time_utils.dart
@@ -16,3 +16,45 @@ String getTimeSinceEpoch(int startTime) {
   return date.toString();
 }
 
+String getTimeAgoEpoch(int startTime) {
+  int now = DateTime.now().toUtc().millisecondsSinceEpoch;
+  int since = startTime * 1000;
+
+  int seconds = (now - since) ~/ 1000;
+  int quantityOfUnit = 0;
+  String timeUnit = 'year';
+
+  if (seconds < 3600) {
+    quantityOfUnit = seconds ~/ 60;
+    timeUnit = 'min';
+  } else if (seconds < 86400) {
+    quantityOfUnit = seconds ~/ 3600;
+    timeUnit = 'hour';
+  } else if (seconds < 604800) {
+    quantityOfUnit = seconds ~/ 86400;
+    timeUnit = 'day';
+  } else if (seconds < 2419200) {
+    quantityOfUnit = seconds ~/ 604800;
+    timeUnit = 'week';
+  } else if (seconds < 29030400) {
+    quantityOfUnit = seconds ~/ 2419200;
+    timeUnit = 'month';
+  } else {
+    quantityOfUnit = seconds ~/ 29030400;
+  }
+
+  //If the unit is more than 1 add 's' char
+  // e.g., 2 hour/day/week, add s at the end
+  if (quantityOfUnit > 1) {
+    timeUnit += 's';
+  }
+
+  return '$quantityOfUnit $timeUnit ago';
+
+  //minutes     60 * 1  = 60
+  //hours       60 * 60 = 3600
+  //days      3600 * 24 = 86400
+  //weeks    86400 * 7  = 604800
+  //months  604800 * 4  = 2419200
+  //years  2419200 * 12 = 29030400
+}
